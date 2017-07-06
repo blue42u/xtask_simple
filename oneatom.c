fa35f9442800e9753ac2602e10853fedbdac72cb
blue42u <blue42u@users.noreply.github.com>
2017-07-06 15:09:12 -0500

    Working on making 1Atom nicer about overflows

--- oneatom.c
+++ oneatom.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 
 static int top;
+static int maxtop;
 static xtask_task** st;
 
 void initQueue(int leaves, int tails, int threads) {
-	st = calloc(leaves+tails+threads, sizeof(xtask_task*));
+	maxtop = leaves + tails + threads-1;
+	st = calloc(maxtop+1, sizeof(xtask_task*));
 	top = threads;
 }
 
@@ -17,16 +19,19 @@ void freeQueue() {
 
 void enqueue(xtask_task* t, int id) {
 	int mine = __sync_add_and_fetch(&top, 1);
+
+	if(mine > maxtop) printf("Overflow!\n");
+
 	while(!__sync_bool_compare_and_swap(&st[mine], NULL, t))
 		pthread_testcancel();
 }
 
 xtask_task* dequeue(int id) {
 	int mine = __sync_fetch_and_sub(&top, 1);
+	xtask_task* t;
 	while(1) {
-		xtask_task* t;
 		while((t = st[mine]) == NULL) pthread_testcancel();
-		if(__sync_bool_compare_and_swap(&st[mine], t, NULL))
-			return t;
+		if(__sync_bool_compare_and_swap(&st[mine], t, NULL)) break;
 	}
+	return t;
 }
