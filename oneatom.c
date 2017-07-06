daebf4ae886a712a0eb51397bc37130064da6169
blue42u <blue42u@users.noreply.github.com>
2017-07-06 16:35:01 -0500

    Comments and clarity (maybe).

--- oneatom.c
+++ oneatom.c
@@ -8,9 +8,9 @@ static int maxtop;
 static xtask_task** st;
 
 void initQueue(int leaves, int tails, int threads) {
-	maxtop = leaves + tails + threads-1;
-	st = calloc(maxtop+1, sizeof(xtask_task*));
-	top = threads;
+	st = calloc(leaves + tails, sizeof(xtask_task*));
+	maxtop = leaves+tails-1;
+	top = 0;
 }
 
 void freeQueue() {
@@ -18,16 +18,21 @@ void freeQueue() {
 }
 
 void enqueue(xtask_task* t, int id) {
+	// Grab a valid index that could be dequeued
 	int mine = __sync_add_and_fetch(&top, 1) % maxtop;
+	// Try to add it to the little stack in that slot
 	do t->sibling = st[mine];
 	while(!__sync_bool_compare_and_swap(&st[mine], t->sibling, t));
 }
 
 xtask_task* dequeue(int id) {
+	// Grab a valid index that will be filled (by enqueue)
 	int mine = __sync_fetch_and_sub(&top, 1) % maxtop;
 	while(1) {
 		xtask_task* t;
+		// Wait for it to have something
 		while((t = st[mine]) == NULL) pthread_testcancel();
+		// Detach the head from the rest of the mini-stack
 		if(__sync_bool_compare_and_swap(&st[mine], t, t->sibling))
 			return t;
 	}
