ebb6be3edf71d434d8822adebdcea3c04ca5d69e
blue42u <blue42u@users.noreply.github.com>
2017-07-09 11:16:11 -0500

    Improve the spinning in oneatom.c by syncing more often when waiting.

--- oneatom.c
+++ oneatom.c
@@ -30,12 +30,12 @@ xtask_task* dequeue(int id) {
 	// Grab a valid index that will be filled (by enqueue)
 	int mine = __sync_fetch_and_sub(&top, 1) % maxtop;
 	if(mine < 0) mine += maxtop;
-	while(1) {
-		xtask_task* t;
-		// Wait for it to have something
-		while((t = st[mine]) == NULL) pthread_testcancel();
-		// Detach the head from the rest of the mini-stack
-		if(__sync_bool_compare_and_swap(&st[mine], t, t->sibling))
-			return t;
-	}
+	xtask_task* t = NULL;
+	do {
+		pthread_testcancel();
+		// Try and pop the top of the mini-stack, if we can.
+		do t = st[mine];
+		while(t&&!__sync_bool_compare_and_swap(&st[mine], t, t->sibling));
+	} while(t == NULL);
+	return t;
 }
