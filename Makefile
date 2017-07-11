383b1f3ac4897e6f1f127bd937289db50d231321
blue42u <blue42u@users.noreply.github.com>
2017-07-11 09:22:00 -0500

    Add a single-stack implementation using atomics.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = clang -O3
 CFLAGS = -std=gnu99 -Wall
 LDFLAGS = -lpthread
 
-all: libxtask-counter.a libxtask-oneatom.a libxtask-jigstack.a
+all: libxtask-counter.a libxtask-oneatom.a libxtask-jigstack.a libxtask-atomstack.a
 
 libxtask-%.a: xtask.o %.o
 	$(AR) rc $@ $^
---
+++ atomstack.c
@@ -0,0 +1,73 @@
+#include "queue.h"
+#include "common.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct {
+	xtask_task* st;
+} Q;
+
+void* initQueue(xtask_config* cfg) {
+	Q* q = malloc(sizeof(Q));
+	q->st = NULL;
+	return q;
+}
+
+void freeQueue(void* vq) {
+	free(vq);
+}
+
+static void rpush(Q* q, xtask_task* t, xtask_task* p) {
+	if(t->child) {
+		// Count the children, using .fate as the after-n counter
+		t->fate = 0;
+		for(xtask_task* c = t->child; c; c = c->sibling) t->fate++;
+		// Set the proper after-n
+		xtask_task* ct = t->child;
+		t->child = p;
+		// Recurse
+		while(ct) {
+			xtask_task* next = ct->sibling;
+			rpush(q, ct, t);
+			ct = next;
+		}
+	} else {
+		// Set the after-n
+		t->child = p;
+		// Push it onto the stack
+		do t->sibling = q->st;
+		while(!__sync_bool_compare_and_swap(&q->st, t->sibling, t));
+	}
+}
+
+void push(void* vq, int tid, xtask_task* tt, xtask_task prev) {
+	while(tt) {
+		xtask_task* next = tt->sibling;
+		rpush(vq, tt, prev.child);
+		tt = next;
+	}
+}
+
+int leaf(void* vq, int tid, xtask_task prev) {
+	xtask_task* t = prev.child;
+	if(!t) return 1;
+	if(__sync_sub_and_fetch(&t->fate, 1) == 0) {
+		xtask_task* p = t->child;
+		t->child = t->sibling = NULL;
+		rpush(vq, t, p);
+	}
+	return 0;
+}
+
+xtask_task* pop(void* vq, int tid) {
+	Q* q = vq;
+	xtask_task* t = NULL;
+	do {
+		pthread_testcancel();
+		// Try and pop from the stack at our index
+		do t = q->st;
+		while(t && !__sync_bool_compare_and_swap(&q->st, t, t->sibling));
+	} while(!t);
+	return t;
+}
